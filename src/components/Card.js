923c9a24e58b559c3b5b449e0ec6c7c0784f0ab6
ginnnnnn <aginlo@hotmail.com>
2019-05-01 23:13:01 +0800

    background size fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ robot, cardSet }) => {
   return (
-    <div className="bg-light-pink dib pa3 ma2 br3 grow bw2 shadow-5 tc">
+    <div className="bg-light-pink dib pa3 ma2 br3 grow bw2 shadow-5 tc w-75 w5-ns">
       <img
         src={`https://robohash.org/set_set${cardSet}/${
           robot.name
