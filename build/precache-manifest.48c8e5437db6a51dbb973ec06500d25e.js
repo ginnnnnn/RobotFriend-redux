fe5c2eb1871c28a9a629aafc6c11a2599d009cf9
ginnnnnn <aginlo@hotmail.com>
2019-05-02 16:23:15 +0800

    build

---
+++ build/precache-manifest.48c8e5437db6a51dbb973ec06500d25e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcde15f39695a583b0a6b0b0147f9501",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ae34889d93e3c936c7ae",
+    "url": "/static/css/main.74442058.chunk.css"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "ae34889d93e3c936c7ae",
+    "url": "/static/js/main.322f989e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ce291af19ea552d448418ddab2627f61",
+    "url": "/static/media/morin-regular-ce29.ce291af1.ttf"
+  }
+]);
